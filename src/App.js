d54dd4b581510f98d106eff5de5c9f87dedf97a4
munhzol <munhzol@gmail.com>
2020-05-11 17:24:23 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [tabs, setTabs] = useState([
+      { id:1, title:'tab1', content:'tab1 content will appear here 1111'},
+      { id:2, title:'tab2', content:'tab2 content will appear here 22'},
+      { id:3, title:'tab3', content:'tab3 content will appear here 3'},
+      { id:4, title:'tab4', content:'tab4 content will appear here '},
+  ]);
+
+  const [seldTab, setSeldTab] = useState(0);
+
+  const selectTab = (e,id) => {
+    setSeldTab(id);
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        tabs.map((tab,i) => 
+          <div onClick={e=>selectTab(e,i)} id={i} className={ seldTab===i?'tabTitle seld':'tabTitle' } key={i}>
+            {tab.title}
+          </div>
+        )
+      }
+      <div className="tabContent">
+        {tabs[seldTab].content}
+      </div>
     </div>
   );
 }
